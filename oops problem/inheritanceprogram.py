499ba10a3e55d06d79337bba24be48eca5361109
https://github.com/anjitha2002/Accurate_python_pratice <anjithaprasadcse@gmail.com>
2025-12-06 12:11:09 +0530

    new program

---
+++ oops problem/inheritanceprogram.py
@@ -0,0 +1,34 @@
+class Bankaccount:
+    def __init__(self):
+        self.balance =int(input ("Enter Balance"))
+    def deposite(self):
+        self.dep_amount=int(input("Enter the amount to deposite"))
+        self.balance +=self.dep_amount
+        print("balance after deposite :",self.balance)
+        return self.balance
+    def withdraw(self):
+        self.with_amount=int(input("enter the amount to withdraw"))
+
+        self.balance -= self.with_amount
+        print("balance after withdrawal",self.balance)
+
+        return self.balance
+
+
+class Minimum_Balance_Account(Bankaccount):
+    def __init__(self):
+        Bankaccount.__init__(self)
+        self.minimum_balance = int(input("Enter the min balance:"))
+    def withdraw1(self):
+        with_amount=int(input("Enter the amount to withdraw"))
+        if self.balance - with_amount< self.minimum_balance:
+            print("Sorry , the min balance should be maintained so withdrawing is not possible ")
+        else:
+            self.with_amount=with_amount
+            Bankaccount.withdraw(self)
+
+m=Minimum_Balance_Account()
+m.withdraw1()
+
+
+
