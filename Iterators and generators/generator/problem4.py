3e84257528e91fe0b8b700f10e8a6046f9850158
https://github.com/anjitha2002/Accurate_python_pratice <anjithaprasadcse@gmail.com>
2025-12-08 12:25:28 +0530

    new program

---
+++ Iterators and generators/generator/problem4.py
@@ -0,0 +1,13 @@
+# Problem 4: Write a function to compute the number of python files (.py extension) in a specified directory recursively.
+import os
+def count_files(path):
+    count=0
+    for entry in os.listdir(path):
+        full_path=os.path.join(path,entry)
+        if os.path.isfile(full_path):
+            if full_path.endswith(".py"):
+                count +=1
+        else:
+            count +=count_files(full_path)
+    return count
+print(count_files(r"C:/Users/ASUS/PycharmProjects/Accurate pratice/Iterators and generators"))
